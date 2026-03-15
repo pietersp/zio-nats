38bac77258ea9b5284fafa1a09b9d5c7c920858c
Pieter Prinsloo <pietersp@gmail.com>
2026-03-15 18:39:36 +0200

    feat: add SerializationFormat trait

---
+++ zio-nats-test/src/test/scala/zio/nats/serialization/SerializationFormatSpec.scala
@@ -0,0 +1,25 @@
+package zio.nats.serialization
+
+import zio._
+import zio.test._
+import zio.test.Assertion._
+import zio.blocks.schema._
+import zio.Chunk
+
+case class Person(name: String, age: Int)
+object Person {
+  implicit val schema: Schema[Person] = Schema.derived
+}
+
+object SerializationFormatSpec extends ZIOSpecDefault {
+  def spec = suite("SerializationFormat")(
+    test("json format encodes and decodes") {
+      val format = SerializationFormat.json
+      val person = Person("Alice", 30)
+      for {
+        encoded <- ZIO.fromEither(format.encode(person))
+        decoded <- ZIO.fromEither(format.decode[Person](encoded))
+      } yield assert(decoded)(equalTo(person))
+    }
+  )
+}
